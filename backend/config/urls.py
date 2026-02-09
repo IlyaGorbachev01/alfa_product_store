a022c0d50a0eb30b20150d73d3d5d01f7059486b
Ilya Gorbachev <ilya-gorbachev01@yandex.ru>
2026-02-09 09:56:34 +0400

    add swagger

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,8 +19,26 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API магазина продуктов",
+        default_version='v1',
+        description="API для интернет-магазина продуктов",
+        contact=openapi.Contact(email="support@example.com"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui(
+        'swagger',
+        cache_timeout=0
+    ), name='schema-swagger-ui'),
 ]
